044b5e9e8cddaac9895162ecd170077e630f33c7
GMatrixGames <gmatrixgames@hotmail.com>
2022-07-02 19:31:30 -0400

    [WIP] Implement changes for UStaticMeshComponent

--- CUE4Parse/UE4/Assets/Exports/Component/USceneComponent.cs
+++ CUE4Parse/UE4/Assets/Exports/Component/USceneComponent.cs
@@ -1,6 +1,28 @@
+using CUE4Parse.UE4.Assets.Readers;
+using CUE4Parse.UE4.Objects.Core.Math;
+using CUE4Parse.UE4.Versions;
+
 namespace CUE4Parse.UE4.Assets.Exports.Component
 {
-    public class USceneComponent : UObject
+    public class USceneComponent : UActorComponent
     {
+        public bool bComputedBoundsOnceForGame;
+        public FBoxSphereBounds Bounds;
+        
+        public override void Deserialize(FAssetArchive Ar, long validPos)
+        {
+            base.Deserialize(Ar, validPos);
+            bComputedBoundsOnceForGame = GetOrDefault<bool>(nameof(bComputedBoundsOnceForGame));
+
+            if (bComputedBoundsOnceForGame && FUE5PrivateFrostyStreamObjectVersion.Get(Ar) >= FUE5PrivateFrostyStreamObjectVersion.Type.SerializeSceneComponentStaticBounds)
+            {
+                var bIsCooked = Ar.ReadBoolean();
+
+                if (bIsCooked)
+                {
+                    Bounds = Ar.Read<FBoxSphereBounds>();
+                }
+            }
+        }
     }
 }
